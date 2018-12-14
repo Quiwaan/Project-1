06838a4ac3e97184864b502ca0f290e4ca402915
quiwaan <quiwaan@me.com>
2018-12-14 10:23:40 -0800

    forgot to commit

---
+++ Project-1/app.js
@@ -0,0 +1,130 @@
+document.addEventListener('DOMContentLoaded', function(){
+	console.log('DOM was loaded!');
+});
+
+var rows = 10;
+var cols = 10;
+var squareSize = 50;
+
+var gameBoardGrid = document.getElementById("board");
+
+// make the grid columns and rows, got from stackflow
+for (i = 0; i < cols; i++) {
+	for (j = 0; j < rows; j++) {
+		
+		// create a new div HTML element for each grid square 
+		var square = document.createElement("div");
+		gameBoardGrid.appendChild(square);
+		square.setAttribute("data-marked", 'false')
+
+    // give each div element an id 
+		square.id = 'sq' + j + i;	
+
+		square.classList.add("square")
+
+		
+		// multiples of the current row or column number
+		var topPosition = j * squareSize;
+		var leftPosition = i * squareSize;			
+		
+		// place each grid square on the page
+		square.style.top = topPosition + 'px';
+		square.style.left = leftPosition + 'px';						
+	}
+}
+
+
+
+// 0 = square, 1 = ship, 2 = hit, 3 = missed
+
+var gameBoard = [
+[0,0,0,0,0,0,0,1,1,1],
+[0,0,0,1,0,0,0,0,0,0],
+[0,0,0,1,0,0,0,0,0,0],
+[0,0,0,1,0,0,0,0,0,0],
+[0,0,0,1,0,0,0,0,0,0],
+[0,0,0,1,0,0,0,0,0,0],
+[0,1,0,0,1,1,1,1,0,0],
+[0,1,0,0,0,0,0,0,0,0],
+[0,1,0,0,0,0,0,0,0,0],
+[0,0,0,0,1,1,0,0,0,0]
+]
+
+// stores the html collection in an array so we can run forEach on it
+var hits = 0;
+var squareDivs = Array.from(document.getElementsByClassName("square"));
+squareDivs.forEach(function(cell){
+	cell.addEventListener("click", fireHit);
+});
+
+function fireHit(e) {
+	if(e.target.getAttribute("data-marked") === 'false') {
+		console.log("inside fire hit");
+		var x = e.target.id[3];
+		var y = e.target.id[2];
+		if (gameBoard[y][x]) {
+			e.target.style.background = "red";
+			console.log(x,y, 'hit');
+			hits++;
+		} else {
+			e.target.style.background = "blue";
+			console.log(x,y, 'miss');
+		}
+		e.target.setAttribute("data-marked", "true")
+	}
+	if(hits == 17){
+		alert("all ships DEAD");
+	}
+}
+	
+function resetGame() {
+		for (i=0; i<squareDivs.length; i++) {
+			squareDivs[i].innerHTML = "";
+		}
+		squareDivs = [];
+		gameBoard = [];
+}
+
+
+
+// 		if (gameBoard[row][col] == 0){
+// 			e.target.style.background = "blue";
+// 			gameBoard[row][col] = 3;
+
+// 		} else if (gameBoard[row][col] == 1){
+// 			e.target.style.background = "red";
+// 			gameBoard[row][col] = 2;
+
+// 		}
+// 			console.log("click")	
+
+ // e.stopPropagation();
+
+
+
+// 0 = square, 1 = ship, 2 = hit, 3 = missed
+
+// var gameBoard = [
+// 0,0,0,0,0,0,0,1,1,1,
+// 0,0,0,0,0,0,0,0,0,0,
+// 0,0,0,1,0,0,0,0,0,0,
+// 0,0,0,1,0,0,0,0,0,0,
+// 0,0,0,1,0,0,0,0,0,0,
+// 0,0,0,1,0,0,0,0,1,1,
+// 0,0,0,1,0,0,0,0,0,0,
+// 0,1,0,0,1,1,1,1,0,0,
+// 0,1,0,0,0,0,0,0,0,0,
+// 0,1,0,0,0,0,0,0,0,0,
+
+// ];
+
+
+
+
+
+
+
+
+
+
+
