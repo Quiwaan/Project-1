ae69a62ae59dfb05b4da67b94156fe69a71d6c8b
quiwaan <quiwaan@me.com>
2018-12-14 17:00:54 -0800

    able to click other board to place ships

--- Project-1/app.js
+++ Project-1/app.js
@@ -23,6 +23,7 @@ function setShotGrid(){
 
 			square.classList.add("square")
 
+
 			
 			// multiples of the current row or column number
 			var topPosition = j * squareSize;
@@ -42,14 +43,14 @@ function setShipGrid(){
 		for (j = 0; j < rows; j++) {
 			
 			// create a new div HTML element for each grid square 
-			var square = document.createElement("div");
-			gameBoardGrid2.appendChild(square);
-			square.setAttribute("data-marked", 'false')
+			var square2 = document.createElement("div");
+			gameBoardGrid2.appendChild(square2);
+			square2.setAttribute("data-marked2", 'false')
 
 	    // give each div element an id 
-			square.id = 'sq' + j + i;	
+			square2.id = 'sQ' + j + i;	
 
-			square.classList.add("square")
+			square2.classList.add("square2")
 
 			
 			// multiples of the current row or column number
@@ -57,8 +58,8 @@ function setShipGrid(){
 			var leftPosition = i * squareSize;			
 			
 			// place each grid square on the page
-			square.style.top = topPosition + 'px';
-			square.style.left = leftPosition + 'px';						
+			square2.style.top = topPosition + 'px';
+			square2.style.left = leftPosition + 'px';						
 		}
 	}
 }
@@ -77,34 +78,49 @@ var gameBoard = [
 [0,1,0,0,0,0,0,0,0,0],
 [0,1,0,0,0,0,0,0,0,0],
 [0,0,0,0,1,1,0,0,0,0]
-]
+];
+
+var gameBoard2 = [
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0]
+];
+
 
 // stores the html collection in an array so we can run forEach on it
 var hits = 0;
+var ships = 0;
 var squareDivs = Array.from(document.getElementsByClassName("square"));
 squareDivs.forEach(function(cell){
 	cell.addEventListener("click", fireHit);
 });
 
-// function fireHit(e) {
-// 	if(e.target.getAttribute("data-marked") === 'false') {
-// 		console.log("inside fire hit");
-// 		var x = e.target.id[3];
-// 		var y = e.target.id[2];
-// 		if (gameBoard[y][x]) {
-// 			e.target.style.background = "red";
-// 			// console.log(x,y, 'hit');
-// 			hits++;
-// 		} else {
-// 			e.target.style.background = "blue";
-// 			// console.log(x,y, 'miss');
-// 		}
-// 		e.target.setAttribute("data-marked", "true")
-// 	}
-// 	if(hits == 17){
-// 		messages.textContent = "all ships dead you are the winner";
-// 	}
-// }
+function fireHit(e) {
+	if(e.target.getAttribute("data-marked") === 'false') {
+		console.log("inside fire hit");
+		var x = e.target.id[3];
+		var y = e.target.id[2];
+		if (gameBoard[y][x]) {
+			e.target.style.background = "red";
+			// console.log(x,y, 'hit');
+			hits++;
+		} else {
+			e.target.style.background = "blue";
+			// console.log(x,y, 'miss');
+		}
+		e.target.setAttribute("data-marked", "true")
+	}
+	if(hits == 17){
+		messages.textContent = "all ships dead you are the winner";
+	}
+}
 
 
 var resetButton = document.getElementById("reset-game");
@@ -123,46 +139,48 @@ function resetGame(e) {
 
 }
 
- 
+var squareDivs2 = Array.from(document.getElementsByClassName("square2"));
+squareDivs2.forEach(function(cell){
+cell.addEventListener("click", clicks);
+});
+
+
+  
 function clicks(e){
-		var x = e.target.id[3];
-		var y = e.target.id[2];
-		if (gameBoard[y][x]) {
-			console.log(x,y,);
-		}
+	var x = e.target.id[3];
+	var y = e.target.id[2];
+	console.log(x,y);
+	gameBoard2[y][x] = 1;
+	document.getElementById(`sQ${y}${x}`).style.backgroundColor = 'darkgrey';
+	console.log(gameBoard2);
+	ships++
+	if(ships > 17 ){
+		messages.textContent = "Only 17 clicks available to place ships"
+	}
+
 }
 
 
-// 		if (gameBoard[row][col] == 0){
-// 			e.target.style.background = "blue";
-// 			gameBoard[row][col] = 3;
-
-// 		} else if (gameBoard[row][col] == 1){
-// 			e.target.style.background = "red";
-// 			gameBoard[row][col] = 2;
-
-// 		}
-// 			console.log("click")	
-
- // e.stopPropagation();
 
 
 
-// 0 = square, 1 = ship, 2 = hit, 3 = missed
 
-// var gameBoard = [
-// 0,0,0,0,0,0,0,1,1,1,
-// 0,0,0,0,0,0,0,0,0,0,
-// 0,0,0,1,0,0,0,0,0,0,
-// 0,0,0,1,0,0,0,0,0,0,
-// 0,0,0,1,0,0,0,0,0,0,
-// 0,0,0,1,0,0,0,0,1,1,
-// 0,0,0,1,0,0,0,0,0,0,
-// 0,1,0,0,1,1,1,1,0,0,
-// 0,1,0,0,0,0,0,0,0,0,
-// 0,1,0,0,0,0,0,0,0,0,
 
-// ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
